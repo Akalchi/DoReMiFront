b6cbe32101248d67ae23427d3c940b8bf3e20465
JesusEnjamio <jenjamiob@gmail.com>
2025-02-24 14:01:12 +0100

    Footer implemented

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <footer className="bg-[#141415] py-4 px-6 fixed bottom-0 left-2 right-2 flex items-center rounded-[64px]">
+            <p className="text-white text-sm font-normal leading-[19.2px] break-words ml-4">
+                &copy;2025 DoReMi-Devs
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
