afddd492591cece07bae1168daa5b889b0d505b5
Karina Reyes <karinareyes@Karinas-MacBook-Pro.local>
2025-02-26 11:26:08 +0100

    HomeView done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
+/* import React from 'react';*/
 import './App.css'
 import Header from "./components/Header.jsx";
-
 import Footer from "./components/Footer";
+import HomeView from './components/HomeView.jsx';
 
 function App() {
   return (
     <div>
       <Header />
+      <HomeView/>
       <Footer />
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="bg-[#141415] py-4 px-6 fixed top-0 left-2 right-2 flex items-center justify-start rounded-[64px] z-10">
+    <header className="bg-[#141415] py-4 px-6 fixed top-0 left-2 right-2 flex items-center justify-start rounded-[64px] z-50">
       <ul className="flex space-x-4">
         <li>
           <a href="/" className="hover:underline text-xs md:text-sm text-white">
@@ -24,7 +24,7 @@ export default function Header() {
         </li>
       </ul>
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-bold text-l text-white z-20">
-        DoReMi
+        DoReMi 🎶
       </div>
     </header>
 
